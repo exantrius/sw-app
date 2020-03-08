de0a4643ce4c4353df9af61b66c5d59c1e77d1bd
Andrew Sednev <andrew.sednev@gmail.com>
2020-03-07 19:14:42 -0800

    Add chaining to getting data

--- src/app/service/starwars_people_service_impl.ts
+++ src/app/service/starwars_people_service_impl.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, catchError, startWith, scan, concatMap } from 'rxjs/operators';
 
 import Character from '@app/model/character';
 import {StarwarsPeopleService} from './starwars_people_service';
 
-interface Result {
+interface ResultRespose {
+  count: number;
+  next?: string;
+  previous?: string;
   results: Character[];
 }
+
+type OnePageRequestFunc = (httpClient: HttpClient, url: string) => Observable<ResultRespose>;
 
 /** external api url. */
 const SERVICE_URL = 'https://swapi.co/api/people';
@@ -19,14 +24,49 @@ export class StarwarsPeopleServiceImpl implements StarwarsPeopleService {
 
   constructor(private readonly httpClient: HttpClient) {}
 
+  /** Requests one page. */
+  private static getOnePage(httpClient: HttpClient, url: string): Observable<ResultRespose> {
+    return httpClient.get<ResultRespose>(url);
+  }
+
+  /** Requests chain of pages. */
+  private static batchGet(getter: OnePageRequestFunc, startUrl: string, httpClient: HttpClient): Observable<ResultRespose> {
+    return getter(httpClient, startUrl)
+      .pipe(concatMap((result: ResultRespose) => {
+        return result.next
+          ? StarwarsPeopleServiceImpl.batchGet(getter, result.next, httpClient).pipe(startWith(result))
+          : of(result);
+      }));
+  }
+
   getPersons(): Observable<Character[]> {
-    return this.httpClient.get<Result>(SERVICE_URL)
-      .pipe(map((r: Result) => r.results));
+    return StarwarsPeopleServiceImpl.batchGet(
+              StarwarsPeopleServiceImpl.getOnePage,
+              SERVICE_URL,
+              this.httpClient)
+      .pipe(
+        scan((accumulted, newBatch) => {
+          if (accumulted) {
+            newBatch.results = accumulted.results.concat(newBatch.results);
+          }
+          return newBatch;
+        }),
+        map((r: ResultRespose) => r.results));
   }
 
   searchPersons(name: string): Observable<Character[]> {
-    return this.httpClient.get<Result>(`${SERVICE_URL}?search=${name}`)
-      .pipe(map((r: Result) => r.results));
+    return StarwarsPeopleServiceImpl.batchGet(
+              StarwarsPeopleServiceImpl.getOnePage,
+              `${SERVICE_URL}?search=${name}`,
+              this.httpClient)
+      .pipe(
+        scan((accumulted, newBatch) => {
+          if (accumulted) {
+            newBatch.results = accumulted.results.concat(newBatch.results);
+          }
+          return newBatch;
+        }),
+        map((r: ResultRespose) => r.results));
   }
 
 }
